b3dc88f372c35c8e7fd58a7ae06177780377025d
Piotr Chorościn <p.choroscin@wasko.pl>
2020-02-25 08:51:23 +0100

    Reformat code

--- src/app/dashboard-guard.service.ts
+++ src/app/dashboard-guard.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DashboardGuardService implements CanActivate{
+export class DashboardGuardService implements CanActivate {
 
   constructor(private authService: AuthService, private router: Router) { }
 
     // Dashboard page is only available if user is logged in
-  canActivate(route: import("@angular/router").ActivatedRouteSnapshot, state: import("@angular/router").RouterStateSnapshot): boolean | import("@angular/router").UrlTree | import("rxjs").Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
-    if(!this.authService.isLoggedIn){
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+  boolean |UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    if (!this.authService.isLoggedIn) {
       this.router.navigate(['/login']);
     }
-    return this.authService.isLoggedIn
+    return this.authService.isLoggedIn;
   }
 
 }
