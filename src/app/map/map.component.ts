44166a757a92433f1b704b33d5c244ae2690b442
Piotr Chorościn <p.choroscin@wasko.pl>
2020-03-02 12:37:45 +0100

    Modules imported

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Debtor } from '../debtors.service';
+import { Killer } from '../killers.service';
+import Map from 'ol/map';
+import Tile from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
+
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+
+  map: Map;
+
+  @Input() title: string;
+  @Input() debtors: Debtor[];
+  @Input() killers: Killer[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.initializeMap();
+  }
+
+  private initializeMap() {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new Tile({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [37.4, 8.82],
+        zoom: 4
+      })
+    });
+  }
+
+  getCoordinates(person: Debtor | Killer): string[] {
+    return person.location.split('/');
+  }
+
+}
