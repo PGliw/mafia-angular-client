7a38c4785bd99ec076065b009ef4c745b03ebf68
Piotr Chorościn <p.choroscin@wasko.pl>
2020-03-05 13:03:07 +0100

    Add locations to map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, DoCheck } from '@angular/core';
 import { Debtor } from '../debtors.service';
 import { Killer } from '../killers.service';
 import Map from 'ol/map';
 import Tile from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import View from 'ol/View';
-
+import layerVector from 'ol/layer/Vector';
+import sourceVector from 'ol/source/Vector';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { fromLonLat } from 'ol/proj';
+import { Markers } from 'ol/layer';
+import { Style, Icon, Fill, Stroke } from 'ol/style';
 
 @Component({
   selector: 'app-map',
@@ -24,8 +30,10 @@ export class MapComponent implements OnInit, DoCheck {
 
   ngOnInit() {
     this.initializeMap();
+    this.addPoints();
   }
 
+  // otherwise map is blank until resizing broswer window
   ngDoCheck() {
     this.map.updateSize();
   }
@@ -45,8 +53,38 @@ export class MapComponent implements OnInit, DoCheck {
     });
   }
 
-  getCoordinates(person: Debtor | Killer): string[] {
-    return person.location.split('/');
+  private addPoints() {
+    const debotorsLocations = this.debtors.map(debtor => this.getCoordinates(debtor));
+    const killerLocations = this.killers.map(killer => this.getCoordinates(killer));
+    const debtorsLayer = this.locationsToPointsLayer(debotorsLocations, 'assets/images/red_star_transparent_small.png');
+    const killersLayer = this.locationsToPointsLayer(killerLocations, 'assets/images/gun_small.png');
+    this.map.addLayer(debtorsLayer);
+    this.map.addLayer(killersLayer);
+  }
+
+  private locationsToPointsLayer(locations: number[][], iconPath: string) {
+    const markers: Feature[] = locations.map(location => new Feature({
+      geometry: new Point(fromLonLat(location))
+    }));
+    markers.forEach(marker => marker.setStyle(new Style({
+      image: new Icon(
+        {
+          crossOrigin: 'anonymous',
+          src: iconPath
+        }
+      )
+    })));
+    return new layerVector({
+      source: new sourceVector(
+        {
+          features: markers
+        }
+      )
+    });
+  }
+
+  private getCoordinates(person: Debtor | Killer): number[] {
+    return person.location.split('/').map(location => +location);
   }
 
 }
